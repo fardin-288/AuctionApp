4b0a02b71c4318d6ced0dc625d304cb02b2c2dac
SarowerRafin <93825845+SarowerRafin@users.noreply.github.com>
2023-10-16 18:25:04 +0600

    ImageStored

--- app/src/main/java/com/example/auctionapp/HomeFragment.java
+++ app/src/main/java/com/example/auctionapp/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.auctionapp;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,17 +13,18 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -48,220 +50,285 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
-
+import android.content.ContentResolver;
+import android.content.*;
 public class HomeFragment extends Fragment {
-	private itemArray notunitemArray ;
-	private SearchView searchView ;
-	private static final int IMAGE_REQUEST =1;
-	private Uri imageuri;
-	private Uri final_uri;
-	private ImageView imageview;
-	private FirebaseAuth auth;
-
-	public static final int ADD_ITEM_REQUEST_CODE = 1;
+    StorageTask uploadTask;
+    String key;
+    private static final int IMAGE_REQUEST =1;
+    private Uri imageuri;
+    private Item tempItem;
+    private Uri final_uri;
+    private ImageView imageview;
+    private FirebaseAuth auth;
+    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("Upload");
+    StorageReference storageReference = FirebaseStorage.getInstance().getReference("Upload");
+    public static final int ADD_ITEM_REQUEST_CODE = 1;
 //    private ArrayList<Item> itemList = new ArrayList<>();
-	public static ItemAdapter adapter,adapter1;
-	public static ListView listView;
-	public static final int PICK_IMAGE_REQUEST = 1;
-	public ArrayList<Item> temp = new ArrayList<>();
-	void showAddItemDialog(Context context) {
+    public static ItemAdapter adapter;
+    public static ListView listView;
+    private Spinner spinnerCategory;
+    public static final int PICK_IMAGE_REQUEST = 1;
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle("Add Item");
+    private Handler handler = new Handler();
 
-		// Set up the layout for the dialog
-		View viewInflated = LayoutInflater.from(context).inflate(R.layout.dialogue_add_item, null);
-		final EditText itemNameEditText = viewInflated.findViewById(R.id.editTextItemName);
-		final EditText itemNameEditPrice = viewInflated.findViewById(R.id.editStartingPrice);
-		final EditText itemDescriptionText = viewInflated.findViewById(R.id.editDescription);
-		Button buttonUploadPicture = viewInflated.findViewById(R.id.buttonUploadPicture);
-		// Add more EditText fields for description, price, and picture URL if needed
+
+//    EditText itemNameEditText;
+//    EditText itemNameEditPrice ;
+//    EditText itemDescriptionText ;
+    void showAddItemDialog(Context context) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Add Item");
+
+        // Set up the layout for the dialog
+        View   viewInflated = LayoutInflater.from(context).inflate(R.layout.dialogue_add_item, null);
+     final EditText    itemNameEditText = viewInflated.findViewById(R.id.editTextItemName);
+      final EditText    itemNameEditPrice = viewInflated.findViewById(R.id.editStartingPrice);
+       final EditText  itemDescriptionText = viewInflated.findViewById(R.id.editDescription);
+       Spinner   spinnerCategory = viewInflated.findViewById(R.id.spinnerCategory);
+        Button buttonUploadPicture = viewInflated.findViewById(R.id.buttonUploadPicture);
+        // Add more EditText fields for description, price, and picture URL if needed
 //        imageview = findViewById(R.id.productImgView);
 
-		imageview = viewInflated.findViewById(R.id.productImgView);
-		builder.setView(viewInflated);
+        databaseReference= FirebaseDatabase.getInstance().getReference("Upload");
 
-		// Set up the buttons
-		builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// Get the item details from the dialog
-				String itemName = itemNameEditText.getText().toString();
-				String itemPriceString = itemNameEditPrice.getText().toString();
-				Float itemPriceFloat = Float.valueOf(itemPriceString);
-				String itemDescription = itemDescriptionText.getText().toString();
-				String imguri;
-				long currentTime = System.currentTimeMillis();
-				// Add more code to retrieve description, price, and picture URL
+        imageview = viewInflated.findViewById(R.id.productImgView);
+        builder.setView(viewInflated);
 
-				if (!itemName.isEmpty()) {
-					// Create a new Item object and add it to the list
-					Item newItem = new Item(itemName,itemDescription, itemPriceFloat,currentTime,final_uri); // Modify as needed
+        ArrayAdapter<String> adapterCategory = new ArrayAdapter<String>(getContext(),R.layout.category_spinner_text,R.id.categoryTextView,Item.categoryString);
+        spinnerCategory.setAdapter(adapterCategory);
+//        final int category_Item = spinnerCategory.getSelectedItemPosition();
 
-					newItem.setUserid();
+        // Set up the buttons
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Get the item details from the dialog
+                String itemName = itemNameEditText.getText().toString();
+                String itemPriceString = itemNameEditPrice.getText().toString();
+                Float itemPriceFloat = Float.valueOf(itemPriceString);
+                String itemDescription = itemDescriptionText.getText().toString();
+                String imguri;
+                long currentTime = System.currentTimeMillis();
 
-					itemArray.itemList.add(newItem);
-					HomeFragment.adapter.notifyDataSetChanged();
+                if (!itemName.isEmpty()) {
+                     // Create a new Item object and add it to the list
+                    final int category_Item = spinnerCategory.getSelectedItemPosition();
+                    Item newItem = new Item(itemName,itemDescription, itemPriceFloat,currentTime,final_uri,category_Item); // Modify as needed
 
-					itemArray.incrementTotal();
-				}
-				dialog.dismiss();
-			}
-		});
+                    itemArray.itemList.add(newItem);
+                     tempItem=newItem;
+                   key=  newItem.addToDatabase(getContext());
 
-		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.cancel();
-			}
-		});
+                    HomeFragment.adapter.notifyDataSetChanged();
 
-		// Add picture here
-		buttonUploadPicture.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				openImageChooser();
-//                Toast.makeText(context, "This is a Toast message", Toast.LENGTH_SHORT).show();
-////                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//////                startActivityForResult(context, HomeFragment.PICK_IMAGE_REQUEST);
-			}
-		});
+                    itemArray.incrementTotal();
+                    if(uploadTask!=null && uploadTask.isInProgress())
+                    {
+                        Toast.makeText(context.getApplicationContext(), "Upload is in progress", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        saveData();
+                    }
+
+                }
+
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Add picture here
+        buttonUploadPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openImageChooser();
+//                saveData();
+            }
 
 
-		builder.show();
-	}
+        });
 
 
-	void openImageChooser() {
+        builder.show();
+    }
 
-		Intent intent = new Intent();
-		intent.setType("image/*");
-		intent.setAction(Intent.ACTION_GET_CONTENT);
-		startActivityForResult(intent, IMAGE_REQUEST);
-	}
+    public String getFileExtension(Uri imageuri)
+    {
+        ContentResolver contentResolver = this.getActivity().getContentResolver();
+        MimeTypeMap mimeTypeMap= MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(imageuri));
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
-			imageuri = data.getData();
-			imageview.setImageURI(imageuri);
-			final_uri=imageuri;
+    }
+
+
+    void saveData(){
+
+        StorageReference ref= storageReference.child(key);
+
+        ref.putFile(final_uri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Image is stored successfully ", Toast.LENGTH_SHORT).show();
+//                        String uploadId= databaseReference.push().getKey();
+//                        MyItem myItem = new MyItem(tempItem.getName(),tempItem.getDescription(),tempItem.getCategory(),tempItem.getCurrentPrice(),System.currentTimeMillis(),taskSnapshot.getStorage().getDownloadUrl().toString(), auth.getCurrentUser());
+//                         databaseReference.child(key).setValue(myItem);
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Image is not stored successfully" + e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    void openImageChooser() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            imageuri = data.getData();
+            imageview.setImageURI(imageuri);
+            final_uri=imageuri;
 //            profileImageView.setImageURI(imageUri);
 //            uploadImageToFirebase();
-		}
-	}
+        }
+    }
 
 
-	private void uploadImageToFirebase() {
-		if (imageuri != null) {
-			StorageReference storageReference = FirebaseStorage.getInstance().getReference()
-					.child("profile_images") // You can customize this storage path
-					.child(auth.getCurrentUser().getUid() + ".jpg");
+    private void uploadImageToFirebase() {
+        if (imageuri != null) {
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference()
+                    .child("profile_images") // You can customize this storage path
+                    .child(auth.getCurrentUser().getUid() + ".jpg");
 
-			storageReference.putFile(imageuri)
-					.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-						@Override
-						public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-							// Image uploaded successfully
-							// You can save the download URL to Firebase Database or perform other actions
-						}
-					})
-					.addOnFailureListener(new OnFailureListener() {
-						@Override
-						public void onFailure(@NonNull Exception e) {
-							// Handle image upload failure
-						}
-					});
-		}
-	}
-
-
+            storageReference.putFile(imageuri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Image uploaded successfully
+                            // You can save the download URL to Firebase Database or perform other actions
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Handle image upload failure
+                        }
+                    });
+        }
+    }
 
 
-	public static void main(String[] args) {
-
-		HomeFragment myHomeFragment = new HomeFragment();
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-		//Time Update
-		itemArray.ItemUpdateTimeRunnable();
-
-		View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
 
-		if(temp.isEmpty()==false)
-		{
-//			itemArray.itemList.clear();
-			itemArray.itemList.addAll(temp);
-			temp.clear();
-		}
-		// Initialize the adapter and ListView
-		adapter = new ItemAdapter(requireActivity(), itemArray.itemList);
-		listView = rootView.findViewById(R.id.listView);
-		listView.setAdapter(adapter);
-		searchView = rootView.findViewById(R.id.HomeSearchViewId);
+    public static void main(String[] args) {
 
+        HomeFragment myHomeFragment = new HomeFragment();
 
-		searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-			@Override
-			public boolean onQueryTextSubmit(String s) {
-
-				return true;
-			}
-
-			@Override
-			public boolean onQueryTextChange(String s) {
-				ArrayList<Item> newlist = new ArrayList<Item>();
-				if (s.length() > 0) {
-					for (Item x : itemArray.itemList) {
-						if (x.getName().toLowerCase().contains(s.toLowerCase())) {
-							Toast.makeText(getActivity(), x.getName() + " " + x.getDescription(), Toast.LENGTH_SHORT).show();
-							newlist.add(x);
-						}
-						else
-						{
-							temp.add(x);
-						}
-					}
-					itemArray.itemList.clear();
-					itemArray.itemList.addAll(newlist);
-					adapter = new ItemAdapter(getActivity(), itemArray.itemList);
-					//					listView.setTextFilterEnabled(true);
-				}
+//        public void refresh(){
+//            Thread backgroundThread = new Thread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    while (true) {
+//                            myHomeFragment.onCreateView();
+//                        try {
+//                            Thread.sleep(1000); // Sleep for 1 second (adjust as needed)
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//            });
+//            backgroundThread.start();
 //
-				else {
-						itemArray.itemList.addAll(temp);
-//					Collections.sort(temp);
-//					Collections.sort(itemArray.itemList);
-						temp.clear();
+//        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+//        Time Update
+        itemArray.ItemUpdateTimeRunnable();
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+/*        RefreshClass refreshClass = new RefreshClass();
+        refreshClass.refresh(rootView,getActivity());*/
+
+//         Initialize the adapter and ListView
+        adapter = new ItemAdapter(requireActivity(), itemArray.itemList);
+        listView = rootView.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
 
 
-					adapter = new ItemAdapter(requireActivity(), itemArray.itemList);
-				}
-				listView.setAdapter(adapter);
-				return true;
-			}
+        // Handle "Add Item" button click to show a dialog
+        rootView.findViewById(R.id.btnAddItem).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddItemDialog(getContext());
+            }
+        });
 
-		});
 
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                adapter.notifyDataSetChanged();
+                handler.postDelayed(this, 100);
+            }
+        }, 100);
 
+        return rootView;
+    }
+}
 
-		// Handle "Add Item" button click to show a dialog
-		rootView.findViewById(R.id.btnAddItem).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				showAddItemDialog(getContext());
-			}
-		});
+class RefreshClass{
 
-		return rootView;
-	}
+    public void refresh(View rootView,Activity activity){
+        Thread backgroundThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    HomeFragment.adapter = new ItemAdapter(activity, itemArray.itemList);
+                    HomeFragment.listView = rootView.findViewById(R.id.listView);
+                    HomeFragment.listView.setAdapter(HomeFragment.adapter);
+                    try {
+                        Thread.sleep(1000); // Sleep for 1 second (adjust as needed)
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        backgroundThread.start();
+
+    }
 }
